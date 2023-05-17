c546b5b46e73c85c41033da1148d653cea8eb1bb
momengit <51290928+MomenGit@users.noreply.github.com>
2023-05-18 00:07:12 +0300

    Add random color, black color and borders contorlfunctionalities

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const creatorBtn = document.getElementById("creator-btn");
+const normalModeBtn = document.getElementById("normal-mode-btn");
+const randomColorBtn = document.getElementById("random-mode-btn");
+const showBordersBtn = document.getElementById("show-borders-btn");
+let mousePressed = 0;
+let mode = 0;
 
 creatorBtn.addEventListener("click", getUserSizeEntry);
+normalModeBtn.addEventListener("click", (e) => {
+  mode = 0;
+});
+randomColorBtn.addEventListener("click", () => {
+  mode = 1;
+});
+showBordersBtn.addEventListener("click", () => {
+  document.querySelectorAll(".cell").forEach((e) => {
+    e.classList.toggle("cell-remove");
+  });
+});
+
+createGrid(50);
 
 function getUserSizeEntry() {
   let gridSize = 1;
@@ -35,6 +53,25 @@ function createCell(size) {
   let sizing = (1 / size) * 100;
   cell.style.width = `${sizing}vmin`;
   cell.style.height = `${sizing}vmin`;
+  cell.addEventListener("mouseover", (e) => {
+    if (mousePressed) setCellColor(cell);
+  });
+  cell.addEventListener("mousedown", () => {
+    mousePressed = 1;
+    setCellColor(cell);
+  });
+  cell.addEventListener("mouseup", () => {
+    mousePressed = 0;
+  });
 
   return cell;
+}
+function setCellColor(cell) {
+  if (mode) {
+    cell.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 255
+    )},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+  } else {
+    cell.style.backgroundColor = "black";
+  }
 }
